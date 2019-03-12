6ed94a9a3c191e9185891053ace08794042f7b86
skek0621 <skek0621@naver.com>
2019-03-12 19:27:59 +0900

    Baek_Joon_11654

---
+++ Baek_Joon_11654.cpp
@@ -0,0 +1,19 @@
+//
+//  Baek_Joon_11654.cpp
+//
+//  Created by Cha Hyun Min on 2019. 3. 12..
+//  Copyright © 2019년 Cha Hyun Min. All rights reserved.
+//  성신여자대학교 IT 학부 20161145 차현민
+
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char a;
+    cin>>a;
+    cout<<(int)a;
+    return 0;
+}
+
