142df0cc8d15cc34e8e10a8fe58e7cf2f1a62881
skek0621 <skek0621@naver.com>
2019-03-22 17:43:00 +0900

    Baek_Joon_11047

--- Greedy/Baek_Joon_11047.cpp
+++ Greedy/Baek_Joon_11047.cpp
@@ -1,6 +1,7 @@
 //
 //  Baek_Joon_11047.cpp
 //  Greedy Algorithm
+//
 //  Created by Cha Hyun Min on 2019. 3. 22..
 //  Copyright © 2019년 Cha Hyun Min. All rights reserved.
 //  성신여자대학교 IT 학부 20161145 차현민
